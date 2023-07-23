d9f86a5dbbebcf454d828a6a2f1384a3dbaafc0f
tidavid1 <hello.tidavid@gmail.com>
2023-07-23 22:20:20 +0900

    INIT: Initialize Commit

---
+++ src/main/java/com/tidavid1/Studywith/StudywithApplication.java
@@ -0,0 +1,13 @@
+package com.tidavid1.Studywith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudywithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudywithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidavid1/Studywith/StudywithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidavid1.Studywith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudywithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
