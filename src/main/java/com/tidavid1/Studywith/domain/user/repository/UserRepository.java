ba15cfade20cf83a7419effc7602a103dd75282e
tidavid1 <hello.tidavid@gmail.com>
2023-08-04 16:40:10 +0900

    FIX: Fix User Controller Params

--- src/main/java/com/tidavid1/Studywith/domain/user/repository/UserRepository.java
+++ src/main/java/com/tidavid1/Studywith/domain/user/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.tidavid1.Studywith.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User u where u.userId = :userId and u.isActive = true")
     Optional<User> findByUserId(@Param("userId") Long userId);
